999b346988081792829ee192d1fd1a97cacfd5d4
Sebastian Uhlig <sebastian-uhlig@outlook.com>
2023-06-24 12:38:48 +0200

    chore: add german language

---
+++ resources/lang/de/filament-webauthn.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'widget' => [
+        'title' => 'Dieses Gerät mit WebAuth registrieren',
+    ],
+    'notifications' => [
+        'unsupported' => 'Diese App wird nicht unterstützt',
+        'registration' => [
+            'success' => 'Dieses Gerät wurde erfolgreich registriert',
+            'error' => 'Dieses Gerät konnte nicht registriert werden',
+        ],
+        'authentication' => [
+            'success' => 'Dieses Gerät wurde erfolgreich registriert',
+            'error' => 'Anmeldung mit diesem Gerät nicht möglich',
+        ],
+    ],
+    'register-button-text' => 'Registrieren',
+    'login-button-text' => 'Mit diesem Gerät anmelden',
+    'or' => 'Oder',
+];
